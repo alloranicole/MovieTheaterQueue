f302c29de9a5db771d8cc82b126729e2c2e6c69e
Geoffrey Perrin <perrig12@highpoint.edu>
2016-01-27 23:41:57 -0500

    I added the outputs

--- testSimulationProg.cpp
+++ testSimulationProg.cpp
@@ -13,7 +13,10 @@ void setSimulationParameters(int& sTime, int& numOfServers,
                              int& transTime,
                              int& tBetweenCArrival);
 
-void CustomerArrival(waitingCustomerQueueType& wCustomers, int tBetweenCArrival,                      int clock, int transTime); 
+void CustomerArrival(waitingCustomerQueueType& wCustomers, 
+                     int tBetweenCArrival, int clock, int transTime);
+
+void updateWaitTime(waitingCustomerQueueType& wCustomers, int& tWait);
 
 void runSimulation();
  
@@ -58,22 +61,35 @@ void CustomerArrival(waitingCustomerQueueType& wCustomers, int tBetweenCArrival,
       CN++;
     }
 }
-    
 
+//Updates tWait so that it also reflects the wait time of customers
+//still waiting in the queue.
+void updateWaitTime(waitingCustomerQueueType& wCustomers, int& tWait)
+{
+     if(!wCustomers.isEmptyQueue())//make sure queue isn't empty
+     {
+          while(!wCustomers.isEmptyQueue())//add front until empty
+          {
+                tWait = tWait + wCustomers.front().getWaitingTime();
+                wCustomers.deleteQueue();
+          }
+     }
+}
 
 void runSimulation()
 {
      int CustomersComplete = 0, sID, tWait = 0, sTime, numServers, transTime,
          tBetweenArrival;
+     double totalCustomers = 0;
      customerType D;
      waitingCustomerQueueType wCustomers;
-   // ofstream outfile;
-    // outfile.open("TheaterSimulation")
-     setSimulationParameters(sTime,numServers,transTime,tBetweenArrival); 
+     // ofstream outfile;
+     // outfile.open("TheaterSimulation")
+     setSimulationParameters(sTime, numServers, transTime, tBetweenArrival); 
      serverListType Servers(numServers);
      srand(time(0));
      for(int clock = 1; clock <= sTime; clock++){
-        // Servers.updateServers(outfile);
+         // Servers.updateServers(outfile);
          Servers.updateServers();
          if(!wCustomers.isEmptyQueue())
              wCustomers.updateWaitingQueue();
@@ -84,15 +100,32 @@ void runSimulation()
                D = wCustomers.front();
                tWait += D.getWaitingTime(); 
                wCustomers.deleteQueue();
-               Servers.setServerBusy(sID,D,
-                                     D.getTransactionTime());
+               Servers.setServerBusy(sID, D, D.getTransactionTime());
             }
          }
      }
-   //  outfile.close();                
-     cout << "Total Wait Time: " << tWait << endl;
-     cout << "Completed Transactions: " << Servers.getCompleted() << endl; 
+
+     //update any final output variables that need it
+     updateWaitTime(wCustomers, tWait);
+     totalCustomers = Servers.getCompleted() + Servers.getNumberOfBusyServers() + 
+                      wCustomers.getCount();
+     
+     //Output original information.      
+     //Note: I like to line up the outputs. If you do not like that,
+     //      feel free to change it back.
+     cout << "---You supplied:--------------------" << endl;
+     cout << "Simulation Time:       " << sTime << endl;
+     cout << "Number of Servers:     " << numServers << endl;
+     cout << "Transaction Time:      " << transTime << endl;
+     //Output result information.
+     cout << "Time Between Arrivals: " << tBetweenArrival << endl;
+     cout << "---The results are:-----------------" << endl;
+     cout << "Total Wait Time:              " << tWait << endl;
+     cout << "Completed Transactions:       " << Servers.getCompleted() << endl;
+     cout << "Customers still with servers: " << Servers.getNumberOfBusyServers() << endl;
      cout << "Customers remaining in queue: " << wCustomers.getCount() << endl;
+     cout << "Average Wait Time:            " << tWait / totalCustomers << endl;
+     //  outfile.close(); 
 }
 
 
