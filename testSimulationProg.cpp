a907439ca461bb4d4ff28de70d6a0171c6355839
Taylor Barkley <andert13@highpoint.edu>
2016-01-28 19:30:35 -0500

    Fixed things that Russell mentioned in class

--- testSimulationProg.cpp
+++ testSimulationProg.cpp
@@ -13,22 +13,24 @@ void setSimulationParameters(int& sTime, int& numOfServers,
                              int& transTime,
                              int& tBetweenCArrival);
 
-void CustomerArrival(waitingCustomerQueueType& wCustomers, 
-                     int tBetweenCArrival, int clock, int transTime);
+bool customerArrival(int tBetweenCArrival);
 
 void updateWaitTime(waitingCustomerQueueType& wCustomers, int& tWait);
 
-void runSimulation(int seed);
+void runSimulation();
  
 int main(int argc, char* argv[])
 {
     //If user inputs a seed it is taken in
     //else -1 is inputted to tell the program to use time(0) instead
     if(argc > 1){
-    int x = atoi(argv[1]);
-    runSimulation(x);
-    } else 
-      runSimulation(-1);
+      int x = atoi(argv[1]);
+      srand((unsigned)x);  
+      runSimulation();
+    } else{ 
+      srand(time(0));
+      runSimulation();
+     }
    
     return 0;
 }
@@ -54,23 +56,18 @@ void setSimulationParameters(int& sTime, int& numOfServers,
     cout << endl;
 }
 
-void CustomerArrival(waitingCustomerQueueType& wCustomers, int tBetweenCArrival,
-                int clock, int transTime){
+bool customerArrival(int tBetweenCArrival){
     //Determine ratio for probability function
     double lambda = (double) 1/tBetweenCArrival;
     double rNum;
-    customerType C;
-    //Counts the customers that arrive
-    static int CN = 1;
     //Random number generated to see if customer arrives
     rNum = ((double) rand() / (RAND_MAX)); 
-    //If customer arrives they are added to the back of the line
-    if(rNum > (exp(-1*lambda))){
-      cout << "Customer " << CN << " arrived at time " << clock << endl; 
-      C.setCustomerInfo(CN,clock,0,transTime);
-      wCustomers.addQueue(C);
-      CN++;
-    }
+    //If customer arrives return true
+    if(rNum > (exp(-1*lambda)))
+      return true;
+    else
+      return false; 
+    
 }
 
 //Updates tWait so that it also reflects the wait time of customers
@@ -86,55 +83,75 @@ void updateWaitTime(waitingCustomerQueueType& wCustomers, int& tWait)
 }
 
 
-void runSimulation(int seed)
+void runSimulation()
 {
      //Variables
      int sID, tWait = 0, sTime, numServers, transTime, tBetweenArrival;
-     double totalCustomers = 0, customersQueue; 
-     customerType D;
+     double customersQueue, customerNum = 0;//Counts the customers that arrive
+     customerType customer;
      waitingCustomerQueueType wCustomers;
 
      //Gets initial values from user
      setSimulationParameters(sTime, numServers, transTime, tBetweenArrival);
      //Sets up the server list with the number of available servers 
-     serverListType Servers(numServers);
-     //Checks if the user input a seed to use or if just using the clock
-     if(seed == -1)
-          srand(time(0));
-     else
-          srand((unsigned)seed);  
+     serverListType servers(numServers);
      //Main clock loop for the simulation
-     for(int clock = 1; clock <= sTime; clock++){
+     for(int clock = 1; clock <= sTime; clock++)
+     {
           //Update the server list to decrement the transaction time of 
           //busy servers
           //Function changed to take out ostream so output prints to screen
-          Servers.updateServers();
+          servers.updateServers();
           //If customers in line, increment waiting time 
           if(!wCustomers.isEmptyQueue())
-               wCustomers.updateWaitingQueue();
+             wCustomers.updateWaitingQueue();
           //If customer arrives add them to the end of the line
-          CustomerArrival(wCustomers,tBetweenArrival,clock,transTime);
+          if(customerArrival(tBetweenArrival))
+          {
+             customerNum++;
+             cout << "Customer " << customerNum << " arrived at time " << clock << endl; 
+             customer.setCustomerInfo(customerNum,clock,0,transTime);
+             wCustomers.addQueue(customer);
+          }
           //If customers in the line and a server is free
           //pair the customer next in line with a free server
           //Check for free server
-          sID = Servers.getFreeServerID();
+          sID = servers.getFreeServerID();
           while(sID != -1 && !wCustomers.isEmptyQueue())
           {
                //Get customer from front of line
-               D = wCustomers.front();
+               customer = wCustomers.front();
                //Add customer waiting time to total
-               tWait += D.getWaitingTime(); 
+               tWait += customer.getWaitingTime(); 
                wCustomers.deleteQueue();
                //Move customer to server
-               Servers.setServerBusy(sID, D, D.getTransactionTime());
-               sID = Servers.getFreeServerID();
+               servers.setServerBusy(sID, customer, customer.getTransactionTime());
+               sID = servers.getFreeServerID();
           }
      }
+    
+/*
+     I believe the way the simulation was already set up does not require this added
+     measure after the loop is over or else when it was created within the printf of
+     updateServers() the created would have made the time unit displayed be clock - 1.
+     Right now if this is added the program will display that the server finished a 
+     transaction at time 101 if that were the case, rather than the time 100. So 
+     personally I do not believe this is needed.
 
+     //Added feature after discussed in class where the first part of the clock loop
+     //is taken care of after the loop ends   
+     //Update the server list to decrement the transaction time of 
+     //busy servers
+     //Function changed to take out ostream so output prints to screen
+     servers.updateServers();
+     //If customers in line, increment waiting time 
+     if(!wCustomers.isEmptyQueue())
+        wCustomers.updateWaitingQueue();
+     
+*/
+     
      //update any final output variables that need it
      customersQueue = wCustomers.getCount();  
-     totalCustomers = Servers.getCompleted() + Servers.getNumberOfBusyServers() +
-                      customersQueue;  
      updateWaitTime(wCustomers, tWait);
      
      //Output original information.      
@@ -149,10 +166,10 @@ void runSimulation(int seed)
      //Output result information.
      cout << "---The results are:-----------------" << endl;
      cout << "Total Wait Time:              " << tWait << endl;
-     cout << "Completed Transactions:       " << Servers.getCompleted() << endl;
-     cout << "Customers still with servers: " << Servers.getNumberOfBusyServers() << endl;
+     cout << "Completed Transactions:       " << servers.getCompleted() << endl;
+     cout << "Customers still with servers: " << servers.getNumberOfBusyServers() << endl;
      cout << "Customers remaining in queue: " << customersQueue << endl;
-     cout << "Average Wait Time:            " << tWait / totalCustomers << endl; 
+     cout << "Average Wait Time:            " << tWait / customerNum << endl; 
      cout << endl; 
 }
 
